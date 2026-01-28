186d0296d6ed2b28a1516653ad024d766b00fa9b
vishwanath44 <chinthavishwanath9@gmail.com>
2026-01-28 22:55:48 +0530

    added new one

---
+++ variable1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1 # no space between value and =
+PERSON2=$2 
+
+echo "$PERSON1:: hello $PERSON2, howw are you?"
+
+echo "$PERSON2:: hello $PERSON1, i am fine, thanks for asking. how are you?"
+
+echo "$PERSON1:: i am doing well, what are you learding $PERSON2"
+
+echo "$PERSON2:: i am learning shell. what about you?"
