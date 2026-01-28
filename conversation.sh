a59cf70cd3b0609ba55a3d64e615ed3b83c33f63
vishwanath44 <chinthavishwanath9@gmail.com>
2026-01-28 22:05:59 +0530

    added new one

--- conversation.sh
+++ conversation.sh
@@ -8,4 +8,4 @@ echo "$PERSON2:: hello $PERSON1, i am fine, thanks for asking. how are you?"
 
 echo "$PERSON1:: i am doing well, what are you learding $PERSON2"
 
-echo "$PERSON2:: "i am learning shell. what about you?"
+echo "$PERSON2:: i am learning shell. what about you?"
