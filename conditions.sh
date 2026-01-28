549ae9f4dad570386036d24e96856fed2bf137b2
vishwanath44 <chinthavishwanath9@gmail.com>
2026-01-29 00:16:07 +0530

    added new one

--- conditions.sh
+++ conditions.sh
@@ -5,7 +5,7 @@ NUMBER=$1
 # -gt  -> greater than
 # -lt  -> less than
 # -eq  -> equal
-# -na  -> not equal
+# -ne  -> not equal
 if [ $NUMBER -gt 20 ]; then
     echo "Given number: $NUMBER is greater than 20"
 if [ $NUMBER -eq 20 ]; then   
