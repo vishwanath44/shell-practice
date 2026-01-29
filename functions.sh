5527161986c269b43ffd76787df51ebe6faf0ca0
vishwanath44 <chinthavishwanath9@gmail.com>
2026-01-29 22:22:01 +0530

    new one added

---
+++ functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$1 ... FAILURE"
+        exit 1
+    else
+        echo "$2 ... Success"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y 
+VALIDATE $? "Installing Mysql"
+
+dnf install node.js -y
+VALIIDATE $? "Installing node.js"
