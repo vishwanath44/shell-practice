3f7cc0c93ec760aa3fade1d87d1dbaf61897fadb
vishwanath44 <chinthavishwanath9@gmail.com>
2026-01-29 23:05:22 +0530

    new one added

---
+++ 14.loops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$1 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... Success" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo sh 14.loops.sh nginx mysql nodejs
+do
+    dnf install $package -y
+    VALIDATE $? "Installation"
+done    
