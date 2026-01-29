089cf2b0d4b466f7d43ed72295ac778efaa6364d
vishwanath44 <chinthavishwanath9@gmail.com>
2026-01-29 22:50:58 +0530

    new one added

--- logs.sh
+++ logs.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -13,10 +13,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$1 ... FAILURE"
+        echo "$1 ... FAILURE" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... Success"
+        echo "$2 ... Success" | tee -a $LOGS_FILE
     fi
 }
 
