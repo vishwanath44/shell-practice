caeb22dc35ed4440a61a61e8f070cc2bbf8b10a9
vishwanath44 <chinthavishwanath9@gmail.com>
2026-02-08 23:02:17 +0530

    added new code

--- backup.sh
+++ backup.sh
@@ -25,11 +25,29 @@ if [ $# -lt 2 ]; then
 fi
 
 if [ ! -d "$SOURCE_DIR" ]; then
-    log "$R $SOURCE_DIR does not exist $N"
+    log "$R Source Directory: $SOURCE_DIR does not exist $N"
     exit 1
 fi
 
 if [ ! -d "$DEST_DIR" ]; then
-    log -e "$R $DEST_DIR does not exist $N"
+    log "$R Destination Directory:  $DEST_DIR does not exist $N"
     exit 1
 fi
+
+### Find the files
+FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +$DAYS)
+
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "Days: $DAYS"
+
+if [ -z "${FILES}" ]; then
+    log "No files to archieve ... $Y Skipping $N"
+else
+    # app-logs-$timestamp.zip
+    log "Files found to archieve: $FILES"
+    TIMESTAMP=$(date +%F-%H-%M-%S)
+    ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
+    log "Archieve name: $ZIP_FILE_NAME"
+    tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
