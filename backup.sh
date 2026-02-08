4cb654216add3ed078ad713f262905d129a6f7ee
vishwanath44 <chinthavishwanath9@gmail.com>
2026-02-08 22:29:42 +0530

    added new code

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ 
+if [ $USERID -ne 0 ]; then
+    echo "$R Please run this script with root user access $N"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e $R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    exit 1
+}
+
+if [ $# -1t 2 ]; then
+    USAGE
+fi
