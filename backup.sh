940789781c6e667b8d8b4a32f4870e2017eaf6a9
vishwanath44 <chinthavishwanath9@gmail.com>
2026-02-08 22:33:18 +0530

    added new code

--- backup.sh
+++ backup.sh
@@ -16,7 +16,7 @@ fi
 mkdir -p $LOGS_FOLDER
 
 USAGE(){
-    echo -e $R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    log "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
     exit 1
 }
 
